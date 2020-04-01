65de3f70f30916f64a4bc15d9aea735122696400
Milos Milosevic <highlander.mm@gmail.com>
2020-04-01 22:59:04 +0200

    Izmena prikaza

---
+++ src/components/appHeaderWithNavigation.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "appHeaderWithNavigation"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/chooseAppearance.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "chooseAppearance"
+}
+</script>
+
+<style scoped>
+
+</style>
